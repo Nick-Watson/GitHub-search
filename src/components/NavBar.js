f5bb2edcf80988838aa249c10019090ef07bae35
Nick-Watson <nickwatson248@gmail.com>
2017-05-29 17:10:38 +0100

    added bootstrap and styled navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 function NavBar () {
     return (
-        <div className=''>
-            Navbar
-        </div>
+        <nav className="navbar navbar-inverse navbar-static-top w-100">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="/">GitHub Search</a>
+                </div>
+                <form className="navbar-form navbar-left" role="search">
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Repository Name"/>
+                    </div>
+                    <button type="submit" className="btn btn-default">
+                        <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+                    </button>
+                </form>
+            </div>
+        </nav>
     );
 }
 
