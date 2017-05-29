d8cf32ba09b87c6971becd197f2ba14c6886f56b
Nick-Watson <nickwatson248@gmail.com>
2017-05-29 16:23:29 +0100

    basic setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+
+class App extends Component {
+    render () {
+        return (
+            <div className='app-containter'>
+                GitHub Search
+            </div>
+        );
+    }
+}
+
+export default App;
